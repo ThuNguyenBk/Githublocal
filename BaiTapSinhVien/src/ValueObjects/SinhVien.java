0bd69890adb5f28a90697fa293f18613164acfd8
Thu <Thu@DESKTOP-OROJIPO>
2018-03-02 20:20:18 +0700

    lan up

---
+++ BaiTapSinhVien/src/ValueObjects/SinhVien.java
@@ -0,0 +1,41 @@
+package ValueObjects;
+
+public class SinhVien {
+	private int imaSV;
+	private String sFullName;
+	private double dDiemSo;
+	private String sXepLoai;
+
+	public int getImaSV() {
+		return imaSV;
+	}
+
+	public void setImaSV(int imaSV) {
+		this.imaSV = imaSV;
+	}
+
+	public String getsFullName() {
+		return sFullName;
+	}
+
+	public void setsFullName(String sFullName) {
+		this.sFullName = sFullName;
+	}
+
+	public double getdDiemSo() {
+		return dDiemSo;
+	}
+
+	public void setdDiemSo(double dDiemSo) {
+		this.dDiemSo = dDiemSo;
+	}
+
+	public String getsXepLoai() {
+		return sXepLoai;
+	}
+
+	public void setsXepLoai(String sXepLoai) {
+		this.sXepLoai = sXepLoai;
+	}
+
+}
